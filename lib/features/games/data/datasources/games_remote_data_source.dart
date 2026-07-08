e4c43237d23cfc336514cc60455a5f0c6c42ad12
Daniel Silva <daniel.silva@example.com>
2026-07-08 09:59:34 +0100

    refactor: usar package http em vez de dio (alinhar com M8/LAB8)

--- lib/features/games/data/datasources/games_remote_data_source.dart
+++ lib/features/games/data/datasources/games_remote_data_source.dart
@@ -1,8 +1,11 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import '../../../../core/constants/api_constants.dart';
+import '../../../../core/constants/api_keys.dart';
 import '../../../../core/error/exceptions.dart';
 import '../models/game_model.dart';
 
+/// Contrato do datasource (camada de dados): vai buscar jogos à RAWG.
 abstract class GamesRemoteDataSource {
   Future<List<GameModel>> getGames({
     String? search,
@@ -14,9 +17,12 @@ abstract class GamesRemoteDataSource {
   Future<List<String>> getScreenshots(int id);
 }
 
+/// Implementação que usa o package http para falar com a API RAWG.
 class GamesRemoteDataSourceImpl implements GamesRemoteDataSource {
-  final Dio dio;
-  GamesRemoteDataSourceImpl(this.dio);
+  GamesRemoteDataSourceImpl({http.Client? client})
+      : _client = client ?? http.Client();
+
+  final http.Client _client;
 
   @override
   Future<List<GameModel>> getGames({
@@ -25,44 +31,51 @@ class GamesRemoteDataSourceImpl implements GamesRemoteDataSource {
     String? genres,
     int page = 1,
   }) async {
-    try {
-      final response = await dio.get(
-        ApiConstants.games,
-        queryParameters: {
-          if (search != null && search.isNotEmpty) 'search': search,
-          if (ordering != null) 'ordering': ordering,
-          if (genres != null) 'genres': genres,
-          'page': page,
-          'page_size': 40,
-        },
-      );
-      final results = response.data['results'] as List<dynamic>;
-      return results
-          .map((json) => GameModel.fromJson(json as Map<String, dynamic>))
-          .toList();
-    } on DioException {
+    final uri = Uri.parse('${ApiConstants.baseUrl}${ApiConstants.games}')
+        .replace(queryParameters: {
+      'key': kRawgApiKey,
+      if (search != null && search.isNotEmpty) 'search': search,
+      if (ordering != null) 'ordering': ordering,
+      if (genres != null) 'genres': genres,
+      'page': '$page',
+      'page_size': '40',
+    });
+
+    final response = await _client.get(uri);
+    if (response.statusCode != 200) {
       throw ServerException();
     }
+    final data = jsonDecode(response.body) as Map<String, dynamic>;
+    final results = data['results'] as List<dynamic>;
+    return results
+        .map((json) => GameModel.fromJson(json as Map<String, dynamic>))
+        .toList();
   }
 
   @override
   Future<GameModel> getGameDetails(int id) async {
-    try {
-      final response = await dio.get('${ApiConstants.games}/$id');
-      return GameModel.fromJson(response.data as Map<String, dynamic>);
-    } on DioException {
+    final uri = Uri.parse('${ApiConstants.baseUrl}${ApiConstants.games}/$id')
+        .replace(queryParameters: {'key': kRawgApiKey});
+
+    final response = await _client.get(uri);
+    if (response.statusCode != 200) {
       throw ServerException();
     }
+    return GameModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   }
 
   @override
   Future<List<String>> getScreenshots(int id) async {
-    try {
-      final response = await dio.get('${ApiConstants.games}/$id/screenshots');
-      final results = response.data['results'] as List<dynamic>;
-      return results.map((s) => s['image'] as String).toList();
-    } on DioException {
+    final uri = Uri.parse(
+            '${ApiConstants.baseUrl}${ApiConstants.games}/$id/screenshots')
+        .replace(queryParameters: {'key': kRawgApiKey});
+
+    final response = await _client.get(uri);
+    if (response.statusCode != 200) {
       throw ServerException();
     }
+    final data = jsonDecode(response.body) as Map<String, dynamic>;
+    final results = data['results'] as List<dynamic>;
+    return results.map((s) => s['image'] as String).toList();
   }
 }
